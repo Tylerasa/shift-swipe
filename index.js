c71805ee8688c10d4f6ef8a69db2aaffac73c854
tylerasa <sarpongasaresylvester@gmail.com>
2021-11-16 03:59:59 +0000

    updating to dynamic port

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express()
 app.get('/', (req, res)=>{
     res.send("Welcome To The Landing Page")
 })
-
-app.listen(3000, ()=>{
+var port = process.env.port || 3000
+app.listen(port, ()=>{
     console.log('listening on PORT 3000')
 })
